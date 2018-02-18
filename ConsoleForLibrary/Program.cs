a89c5f6c2ea7399252f2bd52468f2755a8834c68
Ben <Ben@DESKTOP-5R27RE5>
2018-02-18 13:46:33 +0300

    Add project files.

---
+++ ConsoleForLibrary/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using MyClassLibrary;
+
+namespace ConsoleForLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chooseMenu;
+            Console.WriteLine("Меню программы" + "\n");
+            Console.WriteLine("========================================");
+            Console.WriteLine("1. Журналы (добавить/листать/еще че-то)" + "\n");
+            Console.WriteLine("========================================");
+            Console.WriteLine("========================================");
+            Console.WriteLine("2. Книги (добавить/читать/оставить отзыв)" + "\n");
+            Console.WriteLine("========================================");
+            Console.WriteLine("========================================");
+            Console.WriteLine("3. Учебники (добавить/изучать/выкинуть)" + "\n");
+            Console.WriteLine("========================================");
+            Console.Write("Выберите нужный пункт: ");
+
+            chooseMenu = Convert.ToInt32(Console.ReadLine());
+
+            switch(chooseMenu)
+            {
+                case 1:
+                    Magazine newMagazine = new Magazine();
+
+                    Console.WriteLine("\nСначала добавим журнал\n");
+
+                    Console.Write("Название журнала: ");
+                    newMagazine.PublicationName = Console.ReadLine();
+
+                    Console.Write("Месяц: ");
+                    newMagazine.Month = Console.ReadLine();
+
+                    Console.Write("Номер: ");
+                    newMagazine.No = int.Parse(Console.ReadLine());
+
+                    Console.Write("Кол-во страниц: ");
+                    newMagazine.NumberOfPages = int.Parse(Console.ReadLine());
+
+                    Console.Write("Издательство: ");
+                    newMagazine.PublishingHouseName = Console.ReadLine();
+
+                    Console.Write("Год: ");
+                    newMagazine.PublishingYear = int.Parse(Console.ReadLine());
+
+                    Console.Write("Тираж: ");
+                    newMagazine.Edition = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n============Журнал добавлен!============\n\n");
+                    newMagazine.ShowPrint();
+                    Console.WriteLine("\n========================================");
+                    newMagazine.FlipThrough();
+
+                    break;
+
+                case 2:
+
+                    Book newBook = new Book();
+                    Console.WriteLine("\nСначала добавим книгу\n");
+
+                    Console.Write("Название книги: ");
+                    newBook.PublicationName = Console.ReadLine();
+
+                    Console.Write("Автор: ");
+                    newBook.Author = Console.ReadLine();
+
+                    Console.Write("Жанр: ");
+                    newBook.Genre = Console.ReadLine();
+
+                    Console.Write("Серия: ");
+                    newBook.Seria = Console.ReadLine();
+
+                    Console.Write("Язык: ");
+                    newBook.Language = Console.ReadLine();
+
+                    Console.Write("Переводчик: ");
+                    newBook.Translator = Console.ReadLine();
+
+                    Console.Write("Издательство: ");
+                    newBook.PublishingHouseName = Console.ReadLine();
+
+                    Console.Write("Год: ");
+                    newBook.PublishingYear = int.Parse(Console.ReadLine());
+
+                    Console.Write("Тираж: ");
+                    newBook.Edition = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n============Книга добавлена!============\n\n");
+                    newBook.ShowPrint();
+                    Console.WriteLine("\n========================================");
+                    Console.WriteLine("\nТеперь само чтение\n");
+                    Console.WriteLine("========================================\n");
+                    newBook.Read();
+                    newBook.bookReview();
+
+                    break;
+
+                case 3:
+                    ClassBook newClassBook = new ClassBook();
+                    Console.WriteLine("\nСначала добавим учебник\n");
+
+                    Console.Write("Учебник: ");
+                    newClassBook.PublicationName = Console.ReadLine();
+
+                    Console.Write("Предмет: ");
+                    newClassBook.Subject = Console.ReadLine();
+
+                    Console.Write("Класс: ");
+                    newClassBook.Grade = int.Parse(Console.ReadLine());
+
+                    Console.Write("Год: ");
+                    newClassBook.PublishingYear = int.Parse(Console.ReadLine());
+
+                    Console.Write("Издательство: ");
+                    newClassBook.PublishingHouseName = Console.ReadLine();
+
+                    Console.Write("Тираж: ");
+                    newClassBook.Edition = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\n============Учебник добавлен!============\n\n");
+                    newClassBook.ShowPrint();
+                    Console.WriteLine("\n========================================");
+
+                    break;
+
+                default:
+                    Console.WriteLine("Вы ничего не выбрали или натупили. Конец программы.");
+                    break;
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+//5. Журнал, книга, печатное издание, учебник.
+
+namespace MyClassLibrary
+{
+    public abstract class Print
+    {
+        private int publishing_year;
+        private int edition;
+
+        public string PublicationName { get; set; }
+        public string PublishingHouseName { get; set; }
+        public int PublishingYear
+        {
+            get
+            {
+                return publishing_year;
+            }
+            set
+            {
+                if (value < 1900) publishing_year = 1900;
+                else if (value > 2018) publishing_year = 2018;
+                else publishing_year = value;
+            }
+        }
+        public int Edition
+        {
+            get
+            {
+                return edition;
+            }
+            set
+            {
+                if (value < 1) edition = 1;
+                else edition = value;
+            }
+        }
+
+        public abstract void ShowPrint();
+    }
+       
+    public class Magazine : Print
+    {
+        private int magazine_number;
+        private int magazine_number_of_pages;
+
+        public string Month { get; set; }
+
+        public int No
+        {
+            get
+            {
+                return magazine_number;
+            }
+            set
+            {
+                if (value < 1) magazine_number = 1;
+                else magazine_number = value;
+            }
+        }
+        public int NumberOfPages
+        {
+            get
+            {
+                return magazine_number_of_pages;
+            }
+            set
+            {
+                if (value < 50) magazine_number_of_pages = 50;
+                else if (value > 2000) magazine_number_of_pages = 2000;
+                else magazine_number_of_pages = value;
+            }
+        }
+
+        public void FlipThrough()
+        {
+            Console.Write("Листаю журнал " + PublicationName + "...");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("сплетни");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("...");
+                Console.Write("ещё..");
+                System.Threading.Thread.Sleep(1000);
+                Console.Write("и ещё..");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Well Done! Полистали журнал " + PublicationName + "!");
+        }
+
+        public override void ShowPrint()
+        {
+            Console.WriteLine("Журнал: " + PublicationName + "\n");
+            Console.WriteLine("Номер: " + No + "\n");
+            Console.WriteLine("Месяц: " + Month + "\n");
+            Console.WriteLine("Год: " + PublishingYear + "\n");
+            Console.WriteLine("Издательство: " + PublishingHouseName + "\n");
+            Console.WriteLine("Кол-во страниц: " + NumberOfPages + "\n");
+            Console.WriteLine("Тираж: " + Edition + "\n");
+        }
+    }
+    
+
+
+
+
+
+
+    public class Book : Print
+    {
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Seria { get; set; }
+        public string Language { get; set; }
+        public string Translator { get; set; }
+
+        public void Read()
+        {
+            Console.Write("Читаю книгу " + PublicationName + "...");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("...");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Well Done! Книга " + PublicationName + " прочитана!");
+        }
+        public void bookReview()
+        {
+            Console.Write("\n\nТеперь надо оставить отзыв. Пишите здесь: ");
+            string review = Console.ReadLine();
+            Console.WriteLine("Готово! Спасибо за отзыв - вот так он выглядит:\n");
+            Console.WriteLine("========================================\n");
+            Console.WriteLine(review);
+            Console.WriteLine("========================================\n");
+        }
+
+        public override void ShowPrint()
+        {
+            Console.WriteLine("Книга: " + PublicationName + "\n");
+            Console.WriteLine("Жанр: " + Genre + "\n");
+            Console.WriteLine("Серия: " + Seria + "\n");
+            Console.WriteLine("Автор: " + Author + "\n");
+            Console.WriteLine("Год: " + PublishingYear + "\n");
+            Console.WriteLine("Язык Оригинала: " + Language + "\n");
+            Console.WriteLine("Переводчик: " + Translator + "\n");
+            Console.WriteLine("Издательство: " + PublishingHouseName + "\n");
+            Console.WriteLine("Тираж: " + Edition + "\n");
+        }
+    }
+
+    public class ClassBook : Print
+    {
+        private int classook_grade;
+
+        public string Subject { get; set; }
+        public string Author { get; set; }
+        public int Grade
+        {
+            get
+            {
+                return classook_grade;
+            }
+            set
+            {
+                if (value < 1) classook_grade = 1;
+                else if (value > 11) classook_grade = 11;
+                else classook_grade = value;
+            }
+        }
+
+        public void Learn()
+        {
+            Console.Write("Начинаю изучение...");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write("#");
+                System.Threading.Thread.Sleep(1000);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Well Done! Учебник " + PublicationName + " изучен!");
+        }
+
+        public override void ShowPrint()
+        {
+            Console.WriteLine("Учебник: " + PublicationName + "\n");
+            Console.WriteLine("Предмет: " + Subject + "\n");
+            Console.WriteLine("Класс: " + Grade + "\n");
+            Console.WriteLine("Год: " + PublishingYear + "\n");
+            Console.WriteLine("Издательство: " + PublishingHouseName + "\n");
+            Console.WriteLine("Тираж: " + Edition + "\n");
+        }
+
+
+    }
+}
